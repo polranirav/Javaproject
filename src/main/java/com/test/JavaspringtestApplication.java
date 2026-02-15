6e5efd9425f5c704eba541468bbeb8e8cc66cd78
polranirav <polranirav@gmail.com>
2026-02-15 04:58:49 -0500

    final commit

--- src/main/java/com/test/JavaspringtestApplication.java
+++ src/main/java/com/test/JavaspringtestApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaspringtestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaspringtestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaspringtestApplication.class, args);
+    }
 
 }
+
